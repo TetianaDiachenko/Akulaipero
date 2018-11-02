4326dccb56b4d4e2a617d0829d96fc154ed7f13a
github <tetianamyhun@gmail.com>
2018-11-02 17:24:23 +0200

    add form mailchimp

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
+
+function($) {
+	window.fnames = new Array(); 
+	window.ftypes = new Array();
+	fnames[1]='FNAME';
+	ftypes[1]='text';
+	fnames[4]='PHONE';
+	ftypes[4]='phone';
+	fnames[0]='EMAIL';
+	ftypes[0]='email';
+}(jQuery);
+var $mcj = jQuery.noConflict(true);
